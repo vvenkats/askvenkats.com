5c4decc277db6fbbc908866f8f0a92352988febf
Dmitry Demenchuk <dmitry@demench.uk>
2024-04-28 22:07:18 +0100

    make "About" page

--- app/cv/page.tsx
+++
@@ -1,7 +0,0 @@
-import { genPageMetadata } from 'app/seo'
-
-export const metadata = genPageMetadata({ title: 'CV' })
-
-export default function Page() {
-  return <>TODO: CV HERE</>
-}
--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'Askvenkats' },
-  { href: '/cv', title: 'CV' },
+  { href: '/about', title: 'About' },
   { href: '/blog', title: 'Blog' },
   { href: '/white-papers', title: 'White Papers' },
 ]
