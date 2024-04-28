e0d40b6adb626331288f5f7f7dce1f9ee2dceaa4
Dmitry Demenchuk <dmitry@demench.uk>
2024-04-28 21:51:53 +0100

    make "About" link

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'Askvenkats' },
-  { href: '/cv', title: 'CV' },
+  { href: 'https://venkatvenkatakrishnan.com/', title: 'About' },
   { href: '/blog', title: 'Blog' },
   { href: '/white-papers', title: 'White Papers' },
 ]
