665f8725cd450a39d7670c70683bdc4f45e92a01
Venkata Venkatakrishnan <167224847+vvenkats@users.noreply.github.com>
2024-04-30 21:07:16 +0100

    Update PostLayout.tsx

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -154,7 +154,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                 <Link
                   href={`/${basePath}`}
                   className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                  aria-label="Back to the blog"
+                  aria-label="Back"
                 >
                   &larr; Back to the blog
                 </Link>
